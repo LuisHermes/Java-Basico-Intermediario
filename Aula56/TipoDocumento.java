4a844b3878bd82671565d3150511288c1cf623a0
LuisHermes <luishenriquefrance@gmail.com>
2023-07-21 19:56:47 -0300

    Create TipoDocumento.java

---
+++ Aula56/TipoDocumento.java
@@ -0,0 +1,23 @@
+package Javaintermediario.Aula56;
+
+public enum TipoDocumento {
+	
+	CPF {
+		@Override
+		public String geraNumeroTeste() {
+			
+			return GeraCpfCnpj.cpf();
+		}
+	}, CNPJ {
+		@Override
+		public String geraNumeroTeste() {
+			
+			return GeraCpfCnpj.cnpj();
+			}
+	};
+	
+	public abstract String geraNumeroTeste();
+	
+	
+
+}
