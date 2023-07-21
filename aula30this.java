fe1b4fd553f5f5cba80d5542d9506c5f1d80a1d5
LuisHermes <luishenriquefrance@gmail.com>
2023-07-21 19:54:28 -0300

    Create aula30this.java
    Aula30

---
+++ aula30this.java
@@ -0,0 +1,18 @@
+
+public class aula30this {
+
+	public static void main(String[] args) {
+		
+		
+		String marca;
+		String modelo;
+		int numPassageiros;
+		double capCombustivel;
+		double consumoCombustivel;
+		
+		
+		
+
+	}
+
+}
